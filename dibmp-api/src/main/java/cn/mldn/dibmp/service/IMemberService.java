e3dc1ccf9a69dd9bf56fdb3fd5b87498f33e262a
yxy <778398856@qq.com>
2017-11-05 22:54:01 +0800

    测试

--- dibmp-api/src/main/java/cn/mldn/dibmp/service/IMemberService.java
+++ dibmp-api/src/main/java/cn/mldn/dibmp/service/IMemberService.java
@@ -8,7 +8,7 @@ import cn.mldn.dibmp.vo.Member;
 
 public interface IMemberService {
 	/**
-	 * 根据用户ID获得一个用户的完整对象信息，包含有密码和锁定状态
+	 * 根据用户ID获得一个用户   的完整对象信息，包含有密码和锁定状态
 	 * @param mid 要查询的用户ID
 	 * @return 用户对象，如果用户不存在返回null
 	 */
