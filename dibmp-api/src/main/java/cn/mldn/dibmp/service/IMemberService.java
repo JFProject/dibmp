04319780b8d793c4fc36fb61387a1a3071d75a4a
hh <13096947868@163.com>
2017-11-06 11:19:25 +0800

    我而

--- dibmp-api/src/main/java/cn/mldn/dibmp/service/IMemberService.java
+++ dibmp-api/src/main/java/cn/mldn/dibmp/service/IMemberService.java
@@ -10,7 +10,7 @@ public interface IMemberService {
 	/**
 	 * 根据用户ID获得一个用户   的完整对象信息，包含有密码和锁定状态
 	 * @param mid 要查询的用户ID
-	 * @return 用户对象，如果用户不存在返回null
+	 * @return 用户对象，如果用户不存在返回null  
 	 */
 	public Member get(String mid) ;
 	/**
